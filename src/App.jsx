6190aa952d1846eaeabbd8871b321ba0caa7d6bf
srakesh1019 <srakeshkumar1019@gmail.com>
2023-07-16 22:58:09 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home/home";
+import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer"
